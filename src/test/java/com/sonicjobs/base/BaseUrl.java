70dbc118f827ed7fad0721d6836976088b3ed6ba
Om Satapathy <om.satapthy@sonicjobs.net>
2024-02-23 15:01:26 +0530

    query paraeter changed

--- src/test/java/com/sonicjobs/base/BaseUrl.java
+++ src/test/java/com/sonicjobs/base/BaseUrl.java
@@ -1,18 +1,16 @@
 package com.sonicjobs.base;
 
 public class BaseUrl {
-	
+
 	public static final String stagingBaseurlUk = "https://hunter-uk-master.sonic-dev.net/hunter/api/v2";
 	public static final String stagingBaseurlUS = "https://hunter-us-master.sonic-dev.net/hunter/api/v2";
-	
-	
+
 	public static final String prodBaseurlUk = "https://api.sonicjobs.net/hunter/api/v2";
 	public static final String prodBaseurlUs = "https://api-us2.sonicjobs.net/hunter/api/v2";
-	
-	
+
 	public static String jobseekerToken = "";
 	public static String jobseekerToken2 = "";
-	
+
 	public static String us_jobseekerToken = "";
 	public static String us_jobseekerToken2 = "";
 
